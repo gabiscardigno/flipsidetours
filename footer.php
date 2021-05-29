9d859b6e0b4f2ba4497c39e06378cec572c50f4b
gabiscardigno <79734824+gabiscardigno@users.noreply.github.com>
2021-05-29 16:04:51 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,57 @@
+<footer class="container-fluid footer">
+        <!-- div for the main image to stretch s to the edge of the page-->
+
+        <div class="container">
+            <!-- div to hold the content in the middle of the page-->
+            <div class="row">
+                <!-- a row that gives us access to the BS columns-->
+                <div class="col-md-4 footerleft">
+                    <img src="images/Flipside%20Logo.png" alt="Flipside Logo" height="50">
+                    <p class="address">51 Point Walter Road Fremantle, 6160</p>
+                    <p>(08) 9421 4483</p>
+                </div>
+                <div class="col-md-2 footerlinks">
+                    <p>NAVIGATE</p>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Our Tours</a></li>
+                    </ul><!-- row-->
+                </div>
+                <div class="col-md-2 footerlinks2">
+                    <ul>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Book Now</a></li>
+                    </ul><!-- row-->
+                </div>
+                <div class="col-md-2 footerlinks">
+                    <p>SOCIAL MEDIA</p>
+                    <div class="row">
+                        <div class="container footerimages">
+                            <img src=images/facebookfooter.png> <img src=images/twitterfooter.png> <img src=images/instafooter.png> </div> </div> </div> </div> <!-- container -->
+                        </div><!-- container-fluid-->
+                        <hr>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-3 text-center">
+                                    <p>©2021</p>
+                                </div>
+                                <div class="col-md-3 text-center">
+                                    <p>Terms of Use</p>
+                                </div>
+                                <div class="col-md-3 text-center">
+                                    <p>Privacy Policy</p>
+                                </div>
+                                <div class="col-md-3 text-center">
+                                    <p>Sitemap</p>
+                                </div>
+
+                            </div>
+                        </div>
+
+    </footer>
+</body>
+
+</html>
+<?php get_footer(); ?>
