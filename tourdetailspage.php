03562c58cc759f0a587555fb66471aec73aa4d1a
gabiscardigno <79734824+gabiscardigno@users.noreply.github.com>
2021-06-06 19:20:23 +0800

    Create tourdetailspage.php

---
+++ tourdetailspage.php
@@ -0,0 +1,163 @@
+<?php
+    /*
+    Template Name: Tour Details
+    */
+    ?>
+
+<?php include (TEMPLATEPATH . '/tourheader.php');  /* Tells WordPress to include the smaller header header.php */ ?> 
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 img1">
+                    <img src="http://206.189.45.97/~mesh21/sub/wp-content/uploads/2021/06/headerimg1.jpg" alt="Outside of Fremantle Prison" width="440">
+                </div>
+                <div class="col-md-7 img2">
+                    <img src="http://206.189.45.97/~mesh21/sub/wp-content/uploads/2021/06/headerimg2.jpg" alt="Inside Fremantle Prison" width="638">
+                </div>
+                <div class="col-md-2 img3">
+                    <img src="http://206.189.45.97/~mesh21/sub/wp-content/uploads/2021/06/headerimg3.jpg" alt="Inside Fremantle Prison" width="220">
+                </div>
+            </div>
+        </div>
+
+        <div class="container intro">
+            <p class="text-bold">This is an immersive, hands-on paranormal ghostly tour, where you’ll hunt for real ghosts in known haunted places (Fremantle Prison, Fremantle Cemetery). A professional ghost hunter will lead you through the fundamentals of true ghost hunting, and get a chance to use authentic ghost hunting tools.</p>
+            <div class="col-md-6">
+                <p>We’ll use real ghost hunting equipment including:</p>
+                <ul>
+                    <li>
+                        <p>EMF Ghost Meters</p>
+                    </li>
+                    <li>
+                        <p>Infrared Temperature Detectors</p>
+                    </li>
+                    <li>
+                        <p>Dowsing Rods</p>
+                    </li>
+                    <li>
+                        <p>Hausbell Parabolic Listening Devices</p>
+                    </li>
+                    <li>
+                        <p>SBox Spirit Scanner for Electronic
+                            Voice Phenomena</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-6">
+                <p>At the end of your adventure, you will walk away with the knowledge of what it is to be a real ghost hunter.</p>
+
+                <p>You will have their own EMF reader to use throughout the tour. You’ll get priority access to using tools on the tour, including the dowsing rods.</p>
+            </div>
+        </div>
+    </div>
+
+    <section class="container-fluid tourdetails">
+        <h3>TOUR DETAILS</h3>
+        <div class="container schedule">
+            <p> <span class="text-bold"> • Tours start at 7:00 pm – </span> This is a night time walking tour. Plan to arrive early </p>
+            <p> <span class="text-bold">• Tour Duration – </span> The tour is around 2.5 hours. Unfortunately, the route is not able to accommodate wheelchairs or service animals. </p>
+            <p> <span class="text-bold">• Professional Guides – </span> You will be led by a professional guide who is an expert on
+                hauntings </p>
+            <p> <span class="text-bold"> • Ghost Sightings – </span> This tour regularly produces interactions with spirits. Our dowsing rods are often active, strange sensations are felt, and interesting EMF readings appear. A few friendly ghosts have been known to tag along, too. 7pm start, lasts around 2.5 hours</p>
+
+            <p class="font-italic">Ghost Hunter $59 (includes access to tools, including your own EMF reader during tour)</p>
+        </div>
+
+    </section>
+
+    <div class="container-fluid statsbg">
+        <div class="container">
+            <div class="col-md-6">
+                <p class="statstext">BOOK NOW AND YOU COULD BE ONE OF…</p>
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <p class="statsnumber">200</p>
+                </div>
+                <div class="row">
+                    <img src="http://206.189.45.97/~mesh21/sub/wp-content/uploads/2021/06/statsimg.png" alt="Infographical image of a group of people" class="statsimg" width="445">
+                </div>
+                <div class="row">
+                    <p class="whohave">WHO HAVE ENJOYED THIS TOUR</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="container reviews">
+        <h4>REVIEWS</h4>
+        <div class="container row reviewone">
+            <div class="col-md-4 stars">
+                <span class="fa fa-star checked" data-width="10%"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star"></span>
+            </div>
+            <div class="col-md-2">
+                <p class="reviewname1">- Daniel</p>
+            </div>
+        </div>
+        <div class="container reviewtext">
+            <p>We really enjoyed our tour around Fremantle with Jane. She is very passionate about Fremantle and kept all of us on the tour entertained as we explored and learnt lots about the history of Freo.</p>
+        </div>
+
+        <div class="container reviewtwo">
+            <div class="col-md-4 stars">
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+            </div>
+            <div class="col-md-2">
+                <p class="reviewname2">- Sue</p>
+            </div>
+        </div>
+        <div class="container reviewtext">
+            <p>This was a very enjoyable tour! Getting to finally see the behind the scenes of Fremantle Prison, a place I have always walked past but have heard too many scary stories to go in. Very glad I got up the courage to finally go, was so worth it! Thank you to Josh for being an amazing guide throughout the tour.</p>
+        </div>
+
+    </section>
+
+    <div class="container-fluid instasec">
+        <div class="container instatop">
+            <div class="col-md-2">
+                <img src="http://206.189.45.97/~mesh21/sub/wp-content/uploads/2021/06/instalogo.png" alt="Instagram Logo" height="100">
+            </div>
+            <div class="container instatext">
+                <a class="instalink" href="http://instagram.com">@FLIPSIDE</a>
+                <p>Explore our Instagram hashtag, <span class="hashtag">#flipsideghostly</span>, to see just what you could be experiencing.</p>
+            </div>
+            <div class="container row instaimages">
+                <div class="col-md-3">
+                    <img src="http://206.189.45.97/~mesh21/sub/wp-content/uploads/2021/06/instaimg1.jpg" alt="Fremantle ferris wheel" width="255">
+                </div>
+                <div class="col-md-3">
+                    <img src="http://206.189.45.97/~mesh21/sub/wp-content/uploads/2021/06/instaimg2.jpg" alt="People sitting on the grass listening to live music" width="255">
+                </div>
+                <div class="col-md-3">
+                    <img src="http://206.189.45.97/~mesh21/sub/wp-content/uploads/2021/06/instaimg3.jpg" alt="Kids playing at a locla Fremantle playground" width="255">
+                </div>
+                <div class="col-md-3">
+                    <img src="http://206.189.45.97/~mesh21/sub/wp-content/uploads/2021/06/instaimg4.jpg" alt="Visitors outside Fremantle Prison" width="255">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="container col-md-7 booking">
+            <div class="row">
+                <p class="booktext1">LIKE WHAT YOU SEE?</p>
+            </div>
+            <div class="row">
+                <p class="booktext2">BOOK NOW<span class="exclamation">!</span></p>
+            </div>
+        </div>
+
+        <div class="container co-md-5">
+            <a href="ourtours.html" class="btn-bookend" role="button">BOOK NOW</a>
+        </div>
+    </div>
+<?php get_footer(); ?>  
