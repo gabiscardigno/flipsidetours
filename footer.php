11174c39053cba4e0acd27bb9d2f0d2b4dba3f6e
gabiscardigno <79734824+gabiscardigno@users.noreply.github.com>
2021-06-06 19:21:24 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,16 +6,16 @@
         <div class="row">
             <!-- a row that gives us access to the BS columns-->
             <div class="col-md-4 footerleft">
-                <img src="images/Flipside%20Logo.png" alt="Flipside Logo" height="50">
+                <img src="http://206.189.45.97/~mesh21/sub/wp-content/uploads/2021/06/Flipside-Logo.png" alt="Flipside Logo" height="50">
                 <p class="address">51 Point Walter Road Fremantle, 6160</p>
                 <p>(08) 9421 4483</p>
             </div>
             <div class="col-md-2 footerlinks">
                 <p>NAVIGATE</p>
                 <ul>
-                    <li><a href="#">Home</a></li>
+                    <li><a href="http://206.189.45.97/~mesh21/sub/">Home</a></li>
                     <li><a href="#">About Us</a></li>
-                    <li><a href="#">Our Tours</a></li>
+                    <li><a href="http://206.189.45.97/~mesh21/sub/?page_id=1076">Our Tours</a></li>
                 </ul><!-- row-->
             </div>
             <div class="col-md-2 footerlinks2">
@@ -29,7 +29,7 @@
                 <p>SOCIAL MEDIA</p>
                 <div class="row">
                     <div class="container footerimages">
-                        <img src=images/facebookfooter.png> <img src=images/twitterfooter.png> <img src=images/instafooter.png> </div> </div> </div> </div> <!-- container -->
+                        <img src=http://206.189.45.97/~mesh21/sub/wp-content/uploads/2021/06/facebookfooter.png alt="Facebook Logo"> <img src=http://206.189.45.97/~mesh21/sub/wp-content/uploads/2021/06/twitterfooter.png alt="Twitter Logo"> <img src=http://206.189.45.97/~mesh21/sub/wp-content/uploads/2021/06/instafooter.png alt="Instagram Logo"> </div> </div> </div> </div> <!-- container -->
                     </div><!-- container-fluid-->
                     <hr>
                     <div class="container">
@@ -37,8 +37,8 @@
                             <div class="col-md-3 text-center">
                                 <p>©2021</p>
                             </div>
-                            <div class="col-md-3 text-center">
-                                <p>Terms of Use</p>
+                            <div class="col-md-3 text-center termsfooter">
+                                <a class="termslink" href="http://206.189.45.97/~mesh21/sub/?page_id=1522">Terms of Use</a>
                             </div>
                             <div class="col-md-3 text-center">
                                 <p>Privacy Policy</p>
