681de8ab077406bf3d857b7c2d4aaab5b73520fb
gabiscardigno <79734824+gabiscardigno@users.noreply.github.com>
2021-06-06 19:21:30 +0800

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php
+    /*
+    Template Name: 404 Error
+    */
+    ?>
+
+<?php include (TEMPLATEPATH . '/404header.php');  /* Tells WordPress to include the smaller header header.php */ ?> 
+<div class="conatainer-fluid errorbg">
+        <div class="container">
+            <h1>WHOOPS!</h1>
+            <h2>Looks like you have stumbled across a page that is not quite ready yet</h2>
+            <p>Dont worry! We PINKY PROMISE that it is coming soon so stay tuned! In the meantime, check out our amazing tours that we offer and call on (08) 9421 4483 to get on booking</p>
+            <a href="http://206.189.45.97/~mesh21/sub/?page_id=1076" class="btn btn-error text-center" role="button">VIEW OUR TOURS</a>
+             <div>
+            <img class="ferris text-center" src=http://206.189.45.97/~mesh21/sub/wp-content/uploads/2021/06/ferriswheel.png alt="Ferris Wheel Illustration"> <img>
+        </div>
+</div>
+</div>
+<?php get_footer(); ?>  
